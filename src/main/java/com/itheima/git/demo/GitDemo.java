2d0fa7af2c1ed00ea18811e52dac356865c738ea
jinlongbao <jinlongbao1988@gmail.com>
2019-12-05 19:53:24 +0800

    添加Git测试1

--- src/main/java/com/itheima/git/demo/GitDemo.java
+++ src/main/java/com/itheima/git/demo/GitDemo.java
@@ -3,5 +3,7 @@ package com.itheima.git.demo;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
